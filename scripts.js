cc6397abe5ec947ce80a4ce426be45ab1e2b3016
Aitor Muro Perez <124869947+elmuro2001@users.noreply.github.com>
2025-02-14 22:21:17 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Inicializar todos los popovers
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl);
+  });
+
+  // Inicializar todos los tooltips
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
+
+  
